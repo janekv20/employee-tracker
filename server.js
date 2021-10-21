43b6e1ee5dbbfe29a1632b01814d876f980c1124
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-20 23:04:56 -0500

    writing prompt choices to server.js file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+//packages needed
 const express = require('express');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 const db = require('./db/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
@@ -15,4 +19,29 @@ db.connect(err => {
     });
 });
 
-initializeApp();
+function loadPrompt(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'selection',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'View All Roles', 'View All Departments', 'Add An Employee', 'Add A Role', 'Add A Department', 'Update An Employee Role', 'Quit']
+        }
+    ])
+    .then(selection => {
+        if (selection === "View All Employees") {
+            viewEmployees();
+        } else if (selection === "View All Roles") {
+            viewRoles();
+        } else if (selection === "View All Departments") {
+            viewDepartments();
+        } else if (selection === "Add An Employee") {
+            addEmployee();
+        } else if (selection === "Add A Role") {
+            addRole();
+        } else if (selection === "Add A Department") {
+            addDepartment();
+        }
+    })
+}
+
